9f6a759359ec2aceb3a540e655f33c7ae9b23f79
Eric Conlon <ejconlon@gmail.com>
2022-06-13 13:55:04 -0700

    update

---
+++ scripts/push_deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Trigger a deploy by pushing to the deploy branch
+# Note that this tags the current origin master, so pull and compare before deploying.
+# Use: ./ops/push_deploy.sh [dev]
+
+set -eux
+
+ENVIRONMENT="$1"
+shift
+
+if [[ "${ENVIRONMENT}" != "dev" ]]; then
+    echo "Supported environments: dev"
+    exit 1
+fi
+
+DEPLOY_BRANCH="deploy-${ENVIRONMENT}"
+
+git branch -f ${DEPLOY_BRANCH} origin/master
+git push -f origin ${DEPLOY_BRANCH}
