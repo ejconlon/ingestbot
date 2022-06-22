a4a4adbe70bdeeeea98ce1d6a8824afbf5412cfe
Eric Conlon <ejconlon@gmail.com>
2022-06-22 13:24:56 -0700

    rename github branch name

--- scripts/push_deploy.sh
+++ scripts/push_deploy.sh
@@ -2,19 +2,26 @@
 
 # Trigger a deploy by pushing to the deploy branch
 # Note that this tags the current origin master, so pull and compare before deploying.
-# Use: ./scripts/push_deploy.sh [dev]
+# Use: ./scripts/push_deploy.sh [gh|aws] [dev]
 
 set -eux
 
-ENVIRONMENT="$1"
+PLATFORM="$1"
+ENVIRONMENT="$2"
+shift
 shift
 
-if [[ "${ENVIRONMENT}" != "dev" ]]; then
-  echo "Supported environments: dev"
+if [[ "${PLATFORM}" != "gh" && "${PLATFORM}" != "aws" ]]; then
+  echo "Supported platforms: {gh, aws}"
   exit 1
 fi
 
-DEPLOY_BRANCH="deploy-${ENVIRONMENT}"
+if [[ "${ENVIRONMENT}" != "dev" ]]; then
+  echo "Supported environments: {dev}"
+  exit 1
+fi
+
+DEPLOY_BRANCH="deploy-${PLATFORM}-${ENVIRONMENT}"
 
 git branch -f ${DEPLOY_BRANCH} origin/master
 git push -f origin ${DEPLOY_BRANCH}
